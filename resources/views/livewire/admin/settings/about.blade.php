d5a25597339ce75b67e971776f802626054fc147
Sumon Ahmed <bdsumon4u@gmail.com>
2021-04-20 04:34:05 +0600

    BugFixed: About Section Button Panel

--- resources/views/livewire/admin/settings/about.blade.php
+++ resources/views/livewire/admin/settings/about.blade.php
@@ -37,19 +37,19 @@
         <div class="md:px-2 w-full md:w-1/3">
             <div class="pt-7 pb-2 px-4 bg-white border shadow-md relative rounded-md w-full mt-5">
                 <h2 class="flex bg-white border py-1 px-2 rounded-md absolute left-0 -top-3">
-                    <x:label class="text-sm font-bold" name="button.text" />
+                    <x:label class="text-sm font-bold" name="button_text" />
                 </h2>
-                <x:input name="button.text" wire:model.defer="button.text" class="block w-full px-2 py-1 border rounded-md text-gray-700 bg-gray-100 appearance-none focus:outline-none focus:bg-gray-200 focus:shadow-inner" />
-                <x:error class="text-red-500" name="button.text" />
+                <x:input name="button_text" wire:model.defer="button_text" class="block w-full px-2 py-1 border rounded-md text-gray-700 bg-gray-100 appearance-none focus:outline-none focus:bg-gray-200 focus:shadow-inner" />
+                <x:error class="text-red-500" name="button_text" />
             </div>
         </div>
         <div class="md:px-2 w-full md:w-2/3">
             <div class="pt-7 pb-2 px-4 bg-white border shadow-md relative rounded-md w-full mt-5">
                 <h2 class="flex bg-white border py-1 px-2 rounded-md absolute left-0 -top-3">
-                    <x:label class="text-sm font-bold" name="button.link" />
+                    <x:label class="text-sm font-bold" name="button_link" />
                 </h2>
-                <x:input name="button.link" wire:model.defer="button.link" class="block w-full px-2 py-1 border rounded-md text-gray-700 bg-gray-100 appearance-none focus:outline-none focus:bg-gray-200 focus:shadow-inner" />
-                <x:error class="text-red-500" name="button.link" />
+                <x:input name="button_link" wire:model.defer="button_link" class="block w-full px-2 py-1 border rounded-md text-gray-700 bg-gray-100 appearance-none focus:outline-none focus:bg-gray-200 focus:shadow-inner" />
+                <x:error class="text-red-500" name="button_link" />
             </div>
         </div>
     </div>
